5342585fc060a2019f7a799d3ac68b1065ccaa51
Duncan Smith <duncan.smith@onthebeach.co.uk>
2015-02-20 18:57:47 +0000

    :boom:

--- triangle.rb
+++ triangle.rb
@@ -15,14 +15,12 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
-  case [a, b, c].uniq.count
-  when 1
-    :equilateral
-  when 2
-    :isosceles
-  when 3
-    :scalene
-  end
+
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.size - 1)
+
 end
 
 # Error class used in part 2.  No need to change this code.
